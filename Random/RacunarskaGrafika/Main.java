6cd03e6f0113db53dd17abf1e166ec9c6183b309
Mahir <mahir.suljic@fet.ba>
2025-04-05 22:22:11 +0200

    Changed example

--- Random/RacunarskaGrafika/Main.java
+++ Random/RacunarskaGrafika/Main.java
@@ -2,62 +2,48 @@ public class Main {
 
     public static void main(String[] args) {
         CoordinateSystem2D system = new CoordinateSystem2D();
+
+        system.add(1, -2, Color.RED, 'A');
+        system.add(2, 1, Color.RED, 'B');
+        system.add(2, -2, Color.RED, 'C');
+
         system.draw();
-
-        system.add(1, 1, Color.RED, 'A');
-        system.add(2, 1, Color.GREEN, 'B');
-        system.add(2, 2, Color.BLUE, 'C');
-        system.add(1, 2, Color.YELLOW, 'D');
-        system.add(1, 1.5, Color.WHITE);
-        system.add(2, 1.5, Color.WHITE);
-        system.add(1.5, 1.5, '✖');
-        system.add(1.5, 1);
-        system.add(1.5, 2);
-
+        system.printPoints();
         System.out.println();
+
+        system.rotate(45);
         system.draw();
-
-        system.translate(1, 1);
-
+        system.printPointsDetailed();
         System.out.println();
+
+        system.translate(2, 2);
         system.draw();
+        system.printPointsDetailed();
+        System.out.println();
+
+        system.rotate(45);
+        system.draw();
+        system.printPointsDetailed();
+        System.out.println();
+
+        system.translate(1 - 2 * Math.sqrt(2), -2);
+        system.draw();
+        system.printPoints();
+        System.out.println();
 
         system.rotate(-45);
-
-        System.out.println();
         system.draw();
-
-        system.scale(Math.sqrt(2), Math.sqrt(2));
-
+        system.printPointsDetailed();
         System.out.println();
+
+        system.translate(-1.65, -1.1785);
         system.draw();
-
-        system.translate(-2, 0);
-
+        system.printPointsDetailed();
         System.out.println();
+
+        system.rotate(-45);
         system.draw();
-
-        system.shear(-1, 1);
-
         System.out.println();
-        system.draw();
-
-        system.translate(2, -2);
-
-        System.out.println();
-        system.draw();
-
-        system.scale(0.5, 0.5);
-
-        System.out.println();
-        system.draw();
-
-        system.translate(-1, -1);
-
-        System.out.println();
-        system.draw();
-
-        System.out.println();
-        system.printPoints();
+        system.printVectors();
     }
 }
